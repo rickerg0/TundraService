555327e5e4551f1d59a8c0f497a798c32f0878f3
glgau <glgau@DESKTOP-GODMBGN>
2018-02-07 11:29:19 -0500

    payload object should be serializable.

--- src/main/java/com/tundra/controller/ResponsePayload.java
+++ src/main/java/com/tundra/controller/ResponsePayload.java
@@ -1,9 +1,13 @@
 package com.tundra.controller;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-public class ResponsePayload<T> {
+public class ResponsePayload<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String token;
 
