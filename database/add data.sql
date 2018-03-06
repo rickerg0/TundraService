71c5975544f419c92b94601b31967c76883678c4
glgau <glgau@DESKTOP-GODMBGN>
2018-03-05 19:45:53 -0500

    cleaned up old db scripts and added new test data dump script that is in the correct table order

--- database/add data.sql
+++
@@ -1,5 +0,0 @@
-insert into location (Id,Name,Organization_Id) values(100,"statue, front lobby",1)
-insert into location (Id,Name,Organization_Id) values(101,"An Old Woman Quinten Massys",1)
-
-
-insert into exibittag (Id,Name,Location_Id) values("sw372","7c:ec:79:fc:ed:34-80",100)
